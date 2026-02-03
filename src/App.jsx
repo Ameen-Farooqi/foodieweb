148da9f471807ab616182bfb2c7df2c64d6bae6a
kapilsingh2915 <singhkapil@gmail.com>
2026-02-03 21:31:44 +0530

    kapils commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Blog2 from './pages/Blog2';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import BookTable from "./pages/BookTable";
 export default function App() {
   return (
     <div>
@@ -10,6 +13,9 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/blogs' element={<Blog />} />
         <Route path='/blogs/article' element={<Blog2 />} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+        <Route path="/book-table" element={<BookTable />} />
       </Routes>
     </div>
   );
